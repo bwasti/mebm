a6200763f8501df485c7a5ef9001167036a2429b
Bram Wasti <bwasti@brams-air.myfiosgateway.com>
2021-01-20 12:43:34 -0500

    race cond

--- script.js
+++ script.js
@@ -290,15 +290,18 @@ class ImageLayer extends MoveableLayer {
     this.reader.addEventListener("load", (function() {
       this.img.src = this.reader.result;
       this.img.addEventListener('load', (function() {
-        this.ready = true;
         this.width = this.img.naturalWidth;
         this.height = this.img.naturalHeight;
+        this.ready = true;
       }).bind(this));
     }).bind(this), false);
     this.reader.readAsDataURL(file);
   }
 
   render(ctx_out, ref_time) {
+    if (!this.ready) {
+      return;
+    }
     let f = this.getFrame(ref_time);
     if (f) {
       let scale = f[2];
